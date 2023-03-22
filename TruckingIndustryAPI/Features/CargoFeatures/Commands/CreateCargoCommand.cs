82046c89fc17b29683b03dec871d7c4d2cb45295
lilMaiker <fuhega45@gmail.com>
2023-03-22 15:45:20 +0300

    Добавил обработку типа груза при добавлении груза.

--- TruckingIndustryAPI/Features/CargoFeatures/Commands/CreateCargoCommand.cs
+++ TruckingIndustryAPI/Features/CargoFeatures/Commands/CreateCargoCommand.cs
@@ -30,13 +30,16 @@ namespace TruckingIndustryAPI.Features.CargoFeatures.Commands
                 {
                     // Преобразуем команду в сущность груза
                     var cargo = _mapper.Map<Cargo>(command);
-
+                    cargo.TypeCargo = await _unitOfWork.TypeCargo.GetByIdAsync(cargo.TypeCargoId);
                     // Получаем заявку и трансопрт, связанные с грузом
                     var bid = await _unitOfWork.Bids.GetByIdAsync(cargo.BidsId);
                     var car = await _unitOfWork.Cars.GetByIdAsync(bid.CarsId);
 
                     // Проверяем, может ли трансопрт вместить груз
                     if (!CanFitCargo(car, cargo).Result) return new BadRequestResult() { Error = GetErrorMessage(car, cargo).Result };
+
+                    // Проверяем, может ли трансопрт доставлять такой тип груза
+                    if (!CanSetTypeCargo(car, cargo).Result) return new BadRequestResult() { Error = GetErrorMessage(car, cargo).Result };
 
                     // Добавляем груз и сохраняем изменения
                     await _unitOfWork.Cargo.AddAsync(cargo);
@@ -72,6 +75,20 @@ namespace TruckingIndustryAPI.Features.CargoFeatures.Commands
             }
 
             /// <summary>
+            /// Вспомогательный метод для проверки, можно ли такой тип груза доставлять в трнаспорте
+            /// </summary>
+            /// <param name="car"></param>
+            /// <param name="cargo"></param>
+            /// <returns></returns>
+            private Task<bool> CanSetTypeCargo(Car car, Cargo cargo)
+            {
+                if ((cargo.TypeCargo.NameTypeCargo.Contains("Продукты питания") || cargo.TypeCargo.NameTypeCargo.Contains("Скоропортящийся")) && !car.WithRefrigerator)
+                    return Task.FromResult(false);
+                else
+                    return Task.FromResult(true);
+            }
+
+            /// <summary>
             /// Вспомогательный метод для получения сообщения об ошибке, когда груз не помещается в автомобиль
             /// </summary>
             /// <param name="car"></param>
@@ -79,6 +96,9 @@ namespace TruckingIndustryAPI.Features.CargoFeatures.Commands
             /// <returns></returns>
             private async Task<string> GetErrorMessage(Car car, Cargo cargo)
             {
+                if ((cargo.TypeCargo.NameTypeCargo.Contains("Продукты питания") || cargo.TypeCargo.NameTypeCargo.Contains("Скоропортящийся")) && !car.WithRefrigerator)
+                    return $"В транспорте {car.TrailerNumber} отсутствует холодильник для доставки типа груза {cargo.TypeCargo.NameTypeCargo}.";
+
                 // Получаем общее пространство и вес автомобиля
                 double maxWeightCar = car.MaxWeight;
 
